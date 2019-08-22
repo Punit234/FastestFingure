e96ef14a7ecbc01013c551608afff0fd6952c6a7
WS_MacMini <punit@isummation.com>
2019-08-22 11:22:48 +0530

    Initial Commit

---
+++ Quizzy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzy
+//
+//  Created by WS_MacMini on 22/08/19.
+//  Copyright © 2019 WS_MacMini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
